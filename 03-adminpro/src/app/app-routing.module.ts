528018571d4c2a32db57713987930a0b5c9d13c7
jhonpulido1990 <3825@holbertonstudents.com>
2024-07-05 20:32:10 -0500

    limpieza

--- 03-adminpro/src/app/app-routing.module.ts
+++ 03-adminpro/src/app/app-routing.module.ts
@@ -3,9 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PagesRoutingModule } from './pages/pages.routing';
 
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { AuthRoutingModule } from './auth/auth.routing';
 
