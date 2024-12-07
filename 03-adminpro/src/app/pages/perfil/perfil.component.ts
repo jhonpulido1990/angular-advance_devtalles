a7d5ec7aa2e1e98c21ea9045889af9840f3e9295
jhonpulido1990 <3825@holbertonstudents.com>
2024-12-07 00:37:21 -0500

    fin de la seccion 15

--- 03-adminpro/src/app/pages/perfil/perfil.component.ts
+++ 03-adminpro/src/app/pages/perfil/perfil.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UsuarioService } from '../../services/usuario.service';
 import { PerfilForm } from '../../interfaces/perfil-form.interface';
 import { Usuario } from '../../models/usuario.model';
+import { FileUploadService } from '../../services/file-upload.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-perfil',
@@ -13,6 +15,9 @@ export class PerfilComponent {
   public fb = inject(FormBuilder);
   public usuario!: Usuario;
   private usuarioService = inject(UsuarioService);
+  private fileUploadService = inject(FileUploadService);
+  public imagenSubir!: File;
+  public imgTemp:any = null;
 
   public perfilForm = this.fb.group({
     nombre: ['', [Validators.required]],
@@ -21,16 +26,49 @@ export class PerfilComponent {
 
   constructor() {
     this.usuario = this.usuarioService.usuario;
-    this.perfilForm.reset({nombre: this.usuario.nombre, email: this.usuario.email})
+    this.perfilForm.reset({
+      nombre: this.usuario.nombre,
+      email: this.usuario.email,
+    });
   }
 
   actualizarPerfil() {
     console.log(this.perfilForm.value);
-    this.usuarioService.actualizarPerfil(this.perfilForm.value as PerfilForm)
-      .subscribe( () => {
+    this.usuarioService
+      .actualizarPerfil(this.perfilForm.value as PerfilForm)
+      .subscribe(() => {
         const { nombre, email } = this.perfilForm.value;
         this.usuario.nombre = nombre!;
         this.usuario.email = email!;
+
+        Swal.fire('guardado', 'cambios fueron guardados', 'success')
+      }, (err) => {
+        Swal.fire('Error', err.error.msg, 'error')
+      });
+  }
+
+  cambiarImagen(event: any) {
+    this.imagenSubir = event.target.files[0] as File;
+    if (!this.imagenSubir) {return this.imgTemp = '';}
+
+    const reader = new FileReader();
+    const url64 = reader.readAsDataURL(this.imagenSubir);
+
+    reader.onloadend = () => {
+      this.imgTemp = reader.result;
+      console.log(reader.result)
+    }
+    return this.imgTemp = reader.result;
+  }
+
+  subirImagen() {
+    this.fileUploadService
+      .actualizarFoto(this.imagenSubir, 'usuarios', this.usuario.uid!)
+      .then(img => {
+        this.usuario.img = img;
+        Swal.fire('guardado', 'cambios fueron guardados', 'success');
+      }).catch(err => {
+        Swal.fire('Error', err.error.msg, 'error');
       });
   }
 }
